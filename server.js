6616d0de1c198469fdb9e8dae1cbe7362d671f16
PSH11938532 <42923519+PSH11938532@users.noreply.github.com>
2018-09-03 12:16:32 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ cors = require('cors');
 
 var express = require('express');
 var app = express();
-var APIKEY = "";  //signup at api.openweathermap.org and obtain an API Key
+var APIKEY = "2b27b8de29ebeb22437527350a830e3a";  //signup at api.openweathermap.org and obtain an API Key
 
 var options = {
     host: 'api.openweathermap.org',
